2352dfab7581c1ef1638dc6d189fd4cd551f590d
Prachi Pandey <prachipandey24975@gmail.com>
2026-07-04 21:49:37 +0530

    Time: 7 ms (39.09%), Space: 14.4 MB (21.67%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,7 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        result = 0
+        # result = 0
+        # for num in nums:
+        #     result = result ^ num
+        # return result
+        hashmap = {}
         for num in nums:
-            result = result ^ num
-        return result
+            hashmap[num] = hashmap.get(num, 0) + 1
+        for key in hashmap:
+            if hashmap[key] == 1:
+                return key
