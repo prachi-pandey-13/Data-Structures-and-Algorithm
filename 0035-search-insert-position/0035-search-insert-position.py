cd0a7b3fcae84d3fa78f87ba9ff9da90097b24e1
Prachi Pandey <prachipandey24975@gmail.com>
2026-07-24 18:41:36 +0530

    Time: 0 ms (100%), Space: 12.9 MB (29.31%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        low = 0
+        high = n-1
+        lb = n
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] >= target:
+                lb = mid
+                high = mid-1
+            else:
+                low = mid + 1
+        return lb
