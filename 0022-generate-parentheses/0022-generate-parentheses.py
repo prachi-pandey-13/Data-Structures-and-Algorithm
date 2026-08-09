5c0c45a65fb7d5a72e5f6fc274109b028c0d16b3
Prachi Pandey <prachipandey24975@gmail.com>
2026-08-09 22:57:47 +0530

    Time: 2 ms (33.62%), Space: 19.4 MB (72.71%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        brackets = [""] * (n*2)
+        def solve(index, total, brackets, result):
+            if index >= len(brackets):
+                if total == 0:
+                    result.append("".join(brackets))
+                return
+            if total > len(brackets)//2:
+                return
+            elif total < 0:
+                return
+            brackets[index] = "(" 
+            add = total + 1
+            solve(index+1, add, brackets, result)
+            brackets[index] = ")"
+            add = total - 1
+            solve(index+1, add, brackets, result)
+        solve(0, 0, brackets, result)
+        return result
